fdf1ce8532ccedf1d0bf6251f4ca102a0e964cfc
Kira <78575847+Syren0914@users.noreply.github.com>
2025-08-22 16:00:24 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Looplet',
-  description: 'Created with v0',
+  description: 'Created by looplet',
   generator: 'v0.dev',
 }
 
